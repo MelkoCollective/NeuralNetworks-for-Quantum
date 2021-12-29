49f89d72c001981e7bc183699bac35e90f0b732b
Schuyler Moss <msmoss@uwaterloo.ca>
2021-12-29 11:39:59 -0500

    modifications for next 8x8 experiments. creation of nh24 and nh40 experiments.modifications for next 8x8 experiments. creation of nh24 and nh40 experiments.modifications for next 8x8 experiments. creation of nh24 and nh40 experiments.modifications for next 8x8 experiments. creation of nh24 and nh40 experiments.modifications for next 8x8 experiments. creation of nh24 and nh40 experiments.modifications for next 8x8 experiments. creation of nh24 and nh40 experiments.modifications for next 8x8 experiments. creation of nh24 and nh40 experiments.modifications for next 8x8 experiments. creation of nh24 and nh40 experiments.modifications for next 8x8 experiments. creation of nh24 and nh40 experiments.

--- src_qmc_vmc/experiments/8x8/nh16/VMC_Full/run_vmc.py
+++ src_qmc_vmc/experiments/8x8/nh16/VMC_Full/run_vmc.py
@@ -256,7 +256,7 @@ lr = 0.001     # learning rate of Adam optimizer
 nh = 16        # Number of hidden units in the GRU cell
 ns = 1000     # Number of samples used to approximate the energy at each step
 qmc_epochs = 0 # Training iterations for qmc, if 0 only do vmc
-vmc_epochs = 1000 # Training iterations for vmc, if 0 only do qmc
+vmc_epochs = 4000 # Training iterations for vmc, if 0 only do qmc
 total_epochs = vmc_epochs+qmc_epochs # Total training iterations
 seed = 1234    # Seed of RNG
 batch_size = 100 # Batch size for QMC training
@@ -270,7 +270,7 @@ variance = []
 
 if qmc_epochs != 0:
 
-    path = "../QMC_data"
+    path = "../../../../../QMC_data"
     dim_path = "Dim={}_M=1000000_V={}_omega={}_delta={}".format(Lx, int(V), Omega, delta) # Can change this to look at Dim = 4, 8, 12, 16
     files_we_want = glob.glob(os.path.join(path,dim_path,'samples*'))
     uploaded = {}
