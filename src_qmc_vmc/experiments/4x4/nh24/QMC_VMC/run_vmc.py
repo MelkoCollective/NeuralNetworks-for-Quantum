a04a7f6694a20f259bd98ec8433254ad52661b9c
Schuyler Moss <msmoss@uwaterloo.ca>
2021-12-28 21:54:06 -0500

    updated 4x4 experiments

--- src_qmc_vmc/experiments/4x4/nh24/QMC_VMC/run_vmc.py
+++ src_qmc_vmc/experiments/4x4/nh24/QMC_VMC/run_vmc.py
@@ -253,7 +253,7 @@ delta = 1.0 # Detuning
 
 # RNN-VMC parameters
 lr = 0.001     # learning rate of Adam optimizer
-nh = 8        # Number of hidden units in the GRU cell
+nh = 24        # Number of hidden units in the GRU cell
 ns = 1000     # Number of samples used to approximate the energy at each step
 qmc_epochs = 200 # Training iterations for qmc, if 0 only do vmc
 vmc_epochs = 3800 # Training iterations for vmc, if 0 only do qmc
